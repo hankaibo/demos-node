9b9b5a64b16b1d4082a01ef4edb94ada91f26dd6
hankaibo <hankaibo@hotmail.com>
2018-08-08 17:34:20 +0800

    feat:use ejs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const indexRouter = require("./routes/index");
-const userRouter = require("./routes/users");
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
 
-app.use("/", indexRouter);
-app.use("/users", userRouter);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
 
 app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/:name", function(req, res) {
-  res.send("hello, " + req.params.name);
+router.get('/:name', function(req, res) {
+  res.render('users', {
+    name: req.params.name
+  });
 });
 
 module.exports = router;
