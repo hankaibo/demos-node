5d7b7d919cf1c3b4e6f25f7396e258061017c003
hankaibo <hankaibo@hotmail.com>
2018-08-10 14:11:01 +0800

    feat:post page

---
+++ modules/posts.js
@@ -0,0 +1,56 @@
+const marked = require('marked')
+const Post = require('../lib/mongo').Post
+
+Post.plugin('contentToHtml', {
+  afterFind: function(posts) {
+    return posts.map(function(post) {
+      post.content = marked(post.content)
+      return post
+    })
+  },
+  afterFindOne: function(post) {
+    if (post) {
+      post.content = marked(post.content)
+    }
+    return post
+  }
+})
+
+module.exports = {
+  create: function create(post) {
+    return Post.create(post).exec()
+  },
+  getPostById: function getPostById(postId) {
+    return Post.findOne({ _id: postId })
+      .populate({ path: 'author', model: 'User' })
+      .addCreateAt()
+      .contentToHtml()
+      .exec()
+  },
+  getPosts: function getPosts(author) {
+    const query = {}
+    if (author) {
+      query.author = author
+    }
+    return Post.find(query)
+      .populate({ path: 'author', model: 'User' })
+      .sort({ _id: -1 })
+      .addCreateAt()
+      .contentToHtml()
+      .exec()
+  },
+  incPv: function incPv(postId) {
+    return Post.update({ _id: postId }, { $inc: { pv: 1 } }).exec()
+  },
+  getRawPostById: function getRawPostById(postId) {
+    return Post.findOne({ _id: postId })
+      .populate({ path: 'author', model: 'User' })
+      .exec()
+  },
+  updatePostById: function updatePostById(postId, data) {
+    return Post.update({ _id: postId }, { $set: data }).exec()
+  },
+  delPostById: function delPostById(postId) {
+    return Post.deleteOne({ _id: postId }).exec()
+  }
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,35 +1,149 @@
 const express = require('express')
 const router = express.Router()
 
+const PostModel = require('../modules/posts')
 const checkLogin = require('../middlewares/check').checkLogin
 
 router.get('/', function(req, res, next) {
-  res.render('posts')
+  const author = req.query.author
+
+  PostModel.getPosts(author)
+    .then(function(posts) {
+      res.render('posts', {
+        posts: posts
+      })
+    })
+    .catch(next)
 })
 
 // POST /posts/create
 router.post('/create', checkLogin, function(req, res, next) {
-  res.send('发表文章')
+  const author = req.session.user._id
+  const title = req.fields.title
+  const content = req.fields.content
+
+  try {
+    if (!title.length) {
+      throw new Error('请填写标题')
+    }
+    if (!content.length) {
+      throw new Error('请填写内容')
+    }
+  } catch (e) {
+    req.flash('error', e.message)
+    return res.redirect('back')
+  }
+
+  let post = {
+    author: author,
+    title: title,
+    content: content
+  }
+
+  PostModel.create(post)
+    .then(function(result) {
+      post = result.ops[0]
+      req.flash('success', '发表成功')
+      res.redirect(`/posts/${post._id}`)
+    })
+    .catch(next)
 })
 
 // GET /posts/create
-router.get('create', checkLogin, function(req, res, next) {
-  res.send('发表文章页')
+router.get('/create', checkLogin, function(req, res, next) {
+  res.render('create')
 })
 
 // GET /posts/:postId
 router.get('/:postId', checkLogin, function(req, res, next) {
-  res.send('文章详情页')
+  const postId = req.params.postId
+  Promise.all([PostModel.getPostById(postId), PostModel.incPv(postId)])
+    .then(function(result) {
+      const post = result[0]
+      if (!post) {
+        throw new Error('该文章不存在')
+      }
+      res.render('post', {
+        post: post
+      })
+    })
+    .catch(next)
 })
 
 // GET /posts/:postId/edit
 router.get('/:postId/edit', checkLogin, function(req, res, next) {
-  res.send('更新文章')
+  const postId = req.params.postId
+  const author = req.session.user._id
+
+  PostModel.getRawPostById(postId)
+    .then(function(post) {
+      if (!post) {
+        throw new Error('该文章不存在')
+      }
+      if (author.toString() !== post.author._id.toString()) {
+        throw new Error('权限不足')
+      }
+      res.render('edit', {
+        post: post
+      })
+    })
+    .catch(next)
+})
+
+// POST /posts/:postId/edit
+router.post('/:postId/edit', checkLogin, function(req, res, next) {
+  const postId = req.params.postId
+  const author = req.session.user._id
+  const title = req.fields.title
+  const content = req.fields.content
+
+  try {
+    if (!title.length) {
+      throw new Error('请填写标题')
+    }
+    if (!content.length) {
+      throw new Error('请填写内容')
+    }
+  } catch (e) {
+    req.flash('error', e.message)
+    return res.redirect('back')
+  }
+
+  PostModel.getRawPostById(postId).then(function(post) {
+    if (!post) {
+      throw new Error('文章不存在')
+    }
+    if (post.author._id.toString() !== author.toString()) {
+      throw new Error('没有权限')
+    }
+    PostModel.updatePostById(postId, { title: title, content: content })
+      .then(function() {
+        req.flash('success', '编辑文章成功')
+        res.redirect(`/posts/${postId}`)
+      })
+      .catch(next)
+  })
 })
 
 // GET /posts/:postId/remove
 router.get('/:postId/remove', checkLogin, function(req, res, next) {
-  res.send('删除文章')
+  const postId = req.params.postId
+  const author = req.session.user._id
+
+  PostModel.getRawPostById(postId).then(function(post) {
+    if (!post) {
+      throw new Error('文章不存在')
+    }
+    if (post.author._id.toString() !== author.toString()) {
+      throw new Error('没有权限')
+    }
+    PostModel.delPostById(postId)
+      .then(function() {
+        req.flash('success', '删除成功')
+        res.redirect('/posts')
+      })
+      .catch(next)
+  })
 })
 
 module.exports = router
